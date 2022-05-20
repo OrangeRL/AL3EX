82956848a8be3d323782cd238e6c426b648426bf
tsubasayoshida <k021g1136@g.neec.ac.jp>
2022-05-20 10:54:41 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,151 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+#include<random>
+#include"AxisIndicator.h"
+#include"PrimitiveDrawer.h"
+
+
+
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {
+	
+	delete model_;
+	delete debugCamera_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	model_ = Model::Create();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//x,y,z方向のスケーリングを設定
+	worldTransform_.scale_ = { 1,5,1 };
+	worldTransform_.rotation_ = { 0.0f,0.0f,3.14f/4 };
+
+
+	//スケーリング行列を宣言
+	Matrix4 matScale;
+	/*
+	   Sx, 0, 0, 0
+	   0 ,Sy ,0 ,0
+	   0 ,0 ,Sz ,0
+	   0, 0 ,0 ,1
+	*/
+	matScale.m[0][0] = worldTransform_.scale_.x;
+	matScale.m[1][1] = worldTransform_.scale_.y;
+	matScale.m[2][2] = worldTransform_.scale_.z;
+	matScale.m[3][3] = 1;
+
+
+	/*
+	  cos,sin,0,0
+	  -sin,cos,0,0
+	  0,0,1,0
+	  0,0,0,1
+	*/
+
+	Matrix4 matIdentity;
+	matIdentity.m[0][0] = 1;
+	matIdentity.m[1][1] = 1;
+	matIdentity.m[2][2] = 1;
+	matIdentity.m[3][3] = 1;
+
+	
+	//worldTransform_.matWorld_ *= matScale;
+
+	
+
+	Matrix4 matRotZ = matIdentity;
+	matRotZ.m[0][0] = worldTransform_.rotation_.z;
+	matRotZ.m[0][1] = worldTransform_.rotation_.z;
+	matRotZ.m[1][0] = -worldTransform_.rotation_.z;
+	matRotZ.m[1][1] = worldTransform_.rotation_.z;
+
+
+
+	worldTransform_.matWorld_ = matIdentity;
+	worldTransform_.matWorld_ *= matRotZ;
+	//行列の転送
+	worldTransform_.TransferMatrix();
+
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(10, 10);
+	//軸方向表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+	
+
+
+}
+
+void GameScene::Update() {
+	//デバッグカメラの更新
+	debugCamera_->Update();
+}
+
+void GameScene::Draw() {
+
+	
+
+
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	//3Dモデル描画
+	//model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	//PrimitiveDrawer::GetInstance()->DrawLine3d({ 0,0,0 }, { 10,0,0 }, { 0,0,0,10 });
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
