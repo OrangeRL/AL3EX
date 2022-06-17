f34bbe582fef5cb34c5f757e15b306882434aa42
OrangeRL <jimkung1@hotmail.com>
2022-06-17 09:53:42 +0900

    Add project files.

---
+++ PlayerBullet.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "assert.h"
+#include "Model.h"
+#include "WorldTransform.h"
+
+///<summary>
+///自キャラの弾
+///</summary>
+class PlayerBullet
+{
+	
+public:
+
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	PlayerBullet();
+
+	///<summary>
+	///初期化
+	///</summary>
+	///<paramname="model">モデル</param>
+	///<paramname="position">初期座標</param>
+	void Initialize(Model* model, const Vector3& position);
+
+	///<summary>
+	///更新
+	///</summary>
+	void Update();
+
+	///<summary>
+	///描画
+	///</summary>
+	///<paramname="viewProjection">ビュープロジェクション</param>
+	void Draw(const ViewProjection& viewProjection);
+	void TransferMatrix();
+private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	PlayerBullet* bullet_ = nullptr;
+};
