06fdf3cb6655356177af5645aa3ee8ca6f57ab80
tsubasayoshida <k021g1136@g.neec.ac.jp>
2022-06-29 11:44:27 +0900

    03_07_敵の弾を一定間隔ごとに発射

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,20 +1,41 @@
-#include"Enemy.h"
+#include "Enemy.h"
+
+Vector3 vecM(Vector3& vec, Matrix4& mat) {
+
+	Vector3 vecM = {};
+
+	vec.x = vec.x * mat.m[0][0] + vec.y * mat.m[1][0] + vec.z * mat.m[2][0];
+
+	vec.y = vec.x * mat.m[0][1] + vec.y * mat.m[1][1] + vec.z * mat.m[2][1];
+
+	vec.z = vec.x * mat.m[0][2] + vec.y * mat.m[1][2] + vec.z * mat.m[2][2];
+
+	return vec;
+}
+
 
 Enemy::Enemy() {
 
 }
 
-void Enemy::Initialize(Model* model, const Vector3& position,uint32_t textureHandle) {
+void Enemy::Initialize(Model* model, const Vector3& position, uint32_t textureHandle)
+{
 	//NULLポインタチェック
 	assert(model);
+
+	//引数として受け取ったデータをメンバ変数に記録する
 	model_ = model;
-	//テクスチャ読み込み
 	textureHandle_ = TextureManager::Load("monster.jpg");
-	//ワールドトランスフォームの初期化
+	input_ = Input::GetInstance();
+	debugText_ = DebugText::GetInstance();
+
+	//ワールド変換の初期化
 	worldTransform_.Initialize();
-	// 引数で受け取った初期座標をセット
+	//引数で受け取った初期座標をセット
 	worldTransform_.translation_ = position;
-	
+	worldTransform_.translation_ = { 5.0f,5.0f,10.0f };
+
+	ApproachInit();
 }
 
 void Enemy::TransferMatrix() {
@@ -23,6 +44,7 @@ void Enemy::TransferMatrix() {
 	matIdentity.m[1][1] = 1.0f;
 	matIdentity.m[2][2] = 1.0f;
 	matIdentity.m[3][3] = 1.0f;
+
 
 	Matrix4 matScale;
 	matScale.m[0][0] = worldTransform_.scale_.x;
@@ -75,54 +97,109 @@ void Enemy::TransferMatrix() {
 	worldTransform_.matWorld_ *= matScale;
 	worldTransform_.matWorld_ *= matRot;
 	worldTransform_.matWorld_ *= matTrans;
+
 }
 
-void Enemy::Move() {
+void Enemy::Fire()
+{
 
+	TransferMatrix();
+	//弾の速度
+	const float kBulletSpeed = 1.0f;
+	Vector3 velocity(0, 0, -kBulletSpeed);
+
+	velocity = vecM(velocity, worldTransform_.matWorld_);
+	//弾を生成し、初期化
+	std::unique_ptr<EnemyBullet>newBullet = std::make_unique<EnemyBullet>();
+	newBullet->Initialize(model_, worldTransform_.translation_, velocity);
+	//弾を登録する
+	bullets_.push_back(std::move(newBullet));
+
+}
+
+void Enemy::ApproachInit()
+{
+
+	//発射タイマーを初期化
+
+	// 発射タイマーカウントダウン
+	fireTimer--;
+	// 指定時間に達した
+	if (fireTimer < 0) {
+		// 弾を発射
+		Fire();
+		// 発射タイマーを初期化
+		fireTimer = kFireInterval;
+	}
+
+	debugText_->SetPos(50, 100);
+	debugText_->Printf(
+		"fireTimer:(%f,%f)", fireTimer, kFireInterval);
+}
+
+
+void Enemy::Move()
+{
+	//キャラクターの移動ベクトル
 	Vector3 move = { 0,0,0 };
 
-	// キャラクターの移動速さ
-	const float moveSpeed = 0.2f;
+	float moveSpeed = 0.2f;
+	move = { 0.0f,0.01f,-moveSpeed };
 
-	move = { 0.0f,0.01f,-moveSpeed};
-	
+	//座標移動
 
 	worldTransform_.translation_ += move;
-TransferMatrix();
-//ワールドトランスフォームの更新
+	TransferMatrix();
 	worldTransform_.TransferMatrix();
 }
 
 void Enemy::Update() {
-	Move();
+	//デスフラグの立った弾を削除
+	bullets_.remove_if([](std::unique_ptr<EnemyBullet>& bullet) {
+		return bullet->IsDead();
+		});
+
+	//キャラクターの移動ベクトル
 	Vector3 approachMove = { 0,0,0 };
 	Vector3 leaveMove = { 0,0,0 };
-	float moveSpeed = 0.2f;
+	float moveSpeed = 0.1f;
 	approachMove = { 0.0f,0.0f,-moveSpeed };
 	leaveMove = { -0.1,0.1,-moveSpeed };
-	switch (phase_)
-	{
-	case Enemy::Phase::Approach:
-	default:
-		//移動(ベクトルを加算)
+
+	//Fire();
+	ApproachInit();
+	//弾更新
+	for (std::unique_ptr<EnemyBullet>& bullet : bullets_) { bullet->Update(); }
+	switch (phase_) {
+	case Phase::Approach:
+	default://移動（ベクトルを加算）
 		worldTransform_.translation_ += approachMove;
 		//規定の位置に到達したら離脱
-		if (worldTransform_.translation_.z<0.0f)
-		{
+		if (worldTransform_.translation_.z < -10.0f) {
 			phase_ = Phase::Leave;
 		}
 		break;
-	case Enemy::Phase::Leave:
-		//移動(ベクトルを加算)
+	case Phase::Leave://移動（ベクトルを加算）
 		worldTransform_.translation_ += leaveMove;
 		break;
-		
 	}
-	
+
+	worldTransform_.rotation_.y -= 0.02f;
+	worldTransform_.rotation_.x -= 0.02f;
+	TransferMatrix();
+	worldTransform_.TransferMatrix();
+	debugText_->SetPos(50, 80);
+	debugText_->Printf(
+		"Enemy move:(%f,%f,%f)",
+		worldTransform_.translation_.x,
+		worldTransform_.translation_.y,
+		worldTransform_.translation_.z);
+
+
 }
 
-void Enemy::Draw(ViewProjection& viewProjection) {
-
+void Enemy::Draw(ViewProjection& viewProjection)
+{
 #pragma region 背景スプライト描画
 
 #pragma endregion
@@ -130,6 +207,9 @@ void Enemy::Draw(ViewProjection& viewProjection) {
 #pragma region 3Dオブジェクト描画
 	//モデルの描画
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+
+	//弾描画
+	for (std::unique_ptr<EnemyBullet>& bullet : bullets_) { bullet->Draw(viewProjection); }
 #pragma endregion
 
 #pragma region 前景スプライト描画
--- Enemy.h
+++ Enemy.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "assert.h"
 #include "Audio.h"
+#include "DirectXCommon.h"
+#include "DebugText.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-//<summary>
-//敵
-//</summary>
-class Enemy
-{
+#include "DebugCamera.h"
+#include "EnemyBullet.h"
+#include <memory>
+#include<list>
+///<summary>
+///敵
+///</summary>
+class Enemy {
 public:
 	/// <summary>
 	/// コンストクラタ
@@ -19,7 +24,7 @@ public:
 	Enemy();
 
 	///<summary>///初期化///</summary>///<paramname="model">モデル</param>///<paramname="textureHandle">テクスチャハンドル</param>
-	void Initialize(Model* model, const Vector3& position,uint32_t textureHandle);
+	void Initialize(Model* model, const Vector3& position, uint32_t textureHandle);
 	///<summary>
 	///更新
 	///</summary>
@@ -27,24 +32,36 @@ public:
 	///<summary>
 	///描画
 	///</summary>
+	//void Draw(ViewProjection viewProjection_);
 	void Draw(ViewProjection& viewProjection);
-
 	void TransferMatrix();
 	void Move();
+	void Fire();
+	//接近フェーズ初期化
+	void ApproachInit();
+	//発射間隔
+	static const int kFireInterval = 60;
+
+private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	Input* input_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	DebugText* debugText_ = nullptr;
 	//行動フェーズ
 	enum class Phase {
-		Approach,   //接近する
-		Leave,      //離脱する
+		Approach,//接近する
+		Leave,//離脱する
 	};
 	//フェーズ
 	Phase phase_ = Phase::Approach;
-private:
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0u;
-	//3Dモデル
-	Model* model_ = nullptr;
-	//ワールド変換データ
-	WorldTransform worldTransform_;
-	//速度
-	Vector3 velocity_;
+
+	//弾
+	std::list<std::unique_ptr<EnemyBullet>>bullets_;
+
+	//発射タイマー
+	int32_t fireTimer = 0;
 };
